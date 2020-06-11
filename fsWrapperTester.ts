861b42526a1ac8b02f9565fd29327eebc532a117
Jun0413 <jzeng004@e.ntu.edu.sg>
2020-06-10 17:34:33 -0700

    fix sync

--- fsWrapperTester.ts
+++
@@ -1,8 +0,0 @@
-import { make } from './fsWrapper';
-
-let fsw = make('');
-
-fsw.readFile('package-lock.json', (err, d) => {
-    if (err) { console.log(err); }
-    else { console.log(d.toString()); }
-});
